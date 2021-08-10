5f4663c27f02b6e4ba4a68c31610023213ea7d5a
dtcaro96 <dtcaro96@gmail.com>
2021-08-10 13:25:50 -0400

    Add responsive helper and style Colorbox

--- src/styles/ColorBoxStyles.js
+++ src/styles/ColorBoxStyles.js
@@ -1,5 +1,5 @@
 import chroma from 'chroma-js';
-
+import sizes from './sizes'
 
 const styles = {
     colorBox: {
@@ -12,7 +12,19 @@ const styles = {
         marginBottom: '-3.9px',
         "&:hover button": {
             opacity: '1'
-        }
+        },
+        [sizes.down('lg')]: {
+            width: '25%',
+            height: props => (props.showingFullPalette ? '20%' : '50%')
+        },
+        [sizes.down('md')]: {
+            width: '50%',
+            height: props => (props.showingFullPalette ? '10%' : '50%')
+        },
+        [sizes.down('xs')]: {
+            width: '100%',
+            height: props => (props.showingFullPalette ? '5%' : '10%'),
+        },
     },
     copyText: {
         color: props => chroma(props.background).luminance() >= 0.7 ? 'black' : 'white'
